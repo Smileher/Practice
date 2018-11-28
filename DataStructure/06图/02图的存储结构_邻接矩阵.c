9e700f7baa892b33cc3be5053888b53285ecfe0b
Smileher <vchenhe@live.com>
2018-11-29 06:15:05 +0800

    更正

--- DataStructure/06图/02图的存储结构_邻接矩阵.c
+++ DataStructure/06图/02图的存储结构_邻接矩阵.c
@@ -19,14 +19,14 @@ void CreateMGraph(MGraph *G)
     printf("输入顶点数和边数:\n");
     scanf("%d,%d", &G->numVertexes, &G->numEdges); //输入顶点数和边数
     for (i = 0; i < G->numVertexes; i++)           //读入顶点信息，建立顶点表
-        scanf(&G->vexs[i]);
+        scanf("%c", &G->vexs[i]);
     for (i = 0; i < G->numVertexes; i++)
         for (j = 0; j < G->numVertexes; j++)
             G->arc[i][j] - INFINITY;  //邻接矩阵初始化
     for (k = 0; k < G->numEdges; k++) // 读入numEdges条边，建立邻接矩阵
     {
         printf("输入边(vi,vj)上的下标主，下标了和权w:\n");
-        scanf("&d,%d,%d", &i, &j, &w); //输入边(vi,vj)上的权w
+        scanf("%d,%d,%d", &i, &j, &w); //输入边(vi,vj)上的权w
         G->arc[i][j] = w;
         G->arc[j][i] = G->arc[i][j]; //因为是无向图，矩阵对称
     }
