18def2705defec1d21ab66232f3ebde65a8bf43b
Smileher <vchenhe@live.com>
2018-11-29 08:13:23 +0800

    完善邻接矩阵

--- DataStructure/06图/02图的存储结构_邻接矩阵.c
+++ DataStructure/06图/02图的存储结构_邻接矩阵.c
@@ -1,4 +1,7 @@
-//Adjacency Matrix
+//Adjacency Matrix邻接矩阵
+//以下代码为无向网(邻接矩阵，无向图，带权)
+//若改为无向图(不带权)，则初始化时 INFINITY 改为0即可
+//若改为有向网，则注释 G->arc[j][i] = G->arc[i][j]; 代码即可
 #include <stdio.h>
 //创建邻接矩阵
 typedef char VertexType; //顶点类型应由用户定义
@@ -12,28 +15,64 @@ typedef struct
     int numVertexes, numEdges;    //图中当前的顶点数和边数
 } MGraph;
 
-//建立无向网图的邻接矩阵表示
+//建立无向网
 void CreateMGraph(MGraph *G)
 {
     int i, j, k, w;
-    printf("输入顶点数和边数:\n");
-    scanf("%d,%d", &G->numVertexes, &G->numEdges); //输入顶点数和边数
-    for (i = 0; i < G->numVertexes; i++)           //读入顶点信息，建立顶点表
+    printf("请输入顶点数:");
+    scanf("%d", &G->numVertexes); //输入顶点数
+    printf("请输入边数:");
+    scanf("%d", &G->numEdges); //输入边数
+    while (getchar() != '\n')
+        continue;
+    for (i = 0; i < G->numVertexes; i++) //输入顶点的字符，建立顶点表(一维数组)
+    {
+        printf("请输入第%d个顶点的字符:", i + 1);
         scanf("%c", &G->vexs[i]);
+        while (getchar() != '\n')
+            continue;
+    }
     for (i = 0; i < G->numVertexes; i++)
         for (j = 0; j < G->numVertexes; j++)
-            G->arc[i][j] - INFINITY;  //邻接矩阵初始化
-    for (k = 0; k < G->numEdges; k++) // 读入numEdges条边，建立邻接矩阵
+            G->arc[i][j] = INFINITY;  //邻接矩阵初始化
+    for (k = 0; k < G->numEdges; k++) // 读入numEdges条边，建立邻接矩阵(二维数组)
     {
-        printf("输入边(vi,vj)上的下标主，下标了和权w:\n");
-        scanf("%d,%d,%d", &i, &j, &w); //输入边(vi,vj)上的权w
+        printf("输入边(vi,vj)上的下标i，下标j和权w:（例如:0,1,5）\n");
+        scanf("%d,%d,%d", &i, &j, &w); //输入边(vi,vj)的i,j和权w
+        while (getchar() != '\n')
+            continue;
         G->arc[i][j] = w;
         G->arc[j][i] = G->arc[i][j]; //因为是无向图，矩阵对称
     }
 }
-
+//输出无向网
+void print(MGraph G)
+{
+    int i, j, k;
+    k = 0;
+    printf("%5c*******************\n", '\0');
+    printf("%6c", '\0');
+    for (k = 0; k < G.numVertexes; k++)
+    {
+        printf("%6c", G.vexs[k]); //输出顶点
+    }
+    printf("\n");
+    for (i = 0; i < G.numVertexes; i++)
+    {
+        printf("%6c", G.vexs[i]); //输出顶点
+        for (j = 0; j < G.numVertexes; j++)
+        {
+            printf("%6d", G.arc[i][j]); //输出矩阵
+        }
+        printf("\n");
+    }
+    printf("%5c*******************\n", '\0');
+}
 int main(void)
 {
+    MGraph G;         //定义
+    CreateMGraph(&G); //建立
+    print(G);         //输出
     return 0;
 }
 
@@ -52,6 +91,5 @@ int main(void)
   ├─────┬──┴─┬─┴──┬┴───┴───┴───┴───┴───┴──┬┴───┼───┴┬────┬────┤ ┌───┼───┼───┐ ├───┴───┼───┤ E││
   │ Ctrl│    │Alt │         Space         │ Alt│    │    │Ctrl│ │ ← │ ↓ │ → │ │   0   │ . │←─┘│
   └─────┴────┴────┴───────────────────────┴────┴────┴────┴────┘ └───┴───┴───┘ └───────┴───┴───┘
-
   Code is far away from bug with the keyboard protecting.
 */
