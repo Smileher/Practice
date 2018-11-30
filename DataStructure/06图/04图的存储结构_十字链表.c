42dd144d218fb093694e705c653fbac36dbfeb0b
Smileher <vchenhe@live.com>
2018-11-30 22:46:15 +0800

    十字链表

---
+++ DataStructure/06图/04图的存储结构_十字链表.c
@@ -0,0 +1,12 @@
+//Orthogonal List
+//十字链表是把邻接表和逆邻接表相结合
+//顶点表
+//data firstin firstout
+//firstin出边表头指针 firstout入边表头指针
+
+//边表
+//tailvex headvex headlink taillink
+//tailvex表示弧起点在顶点表中的下标
+//headvex表示弧终点在顶点表中的下标
+//headlink表示入边表指针域，指向终点相同的下一条边
+//taillink表示边表指针域，指向起点相同的下一条边
